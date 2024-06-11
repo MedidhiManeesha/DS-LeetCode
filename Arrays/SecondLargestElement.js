010091fd15c7b04c58ca22c9e0e657dda45a2317
Maneesha <medidhimaneesha@gmail.com>
2024-06-11 22:27:12 +0530

    Array day-2

--- Arrays/SecondLargestElement.js
+++ Arrays/SecondLargestElement.js
@@ -1,14 +1,22 @@
-const arr = [23,34,25,66,78];
+const arr = [45,54];
 let first = -Infinity;
 let second = -Infinity;
-for(let i = 0; i< arr.length; i++){
-    if(arr[i]> first){
-        second = first
+let third = -Infinity;
+for(let i =0;i< arr.length;i++){
+    if(arr[i] > first){
+        third = second;
+        second = first;
         first = arr[i];
     }else if(arr[i] > second && arr[i]!= first){
+        third = second;
         second = arr[i];
+    }else if(arr[i] > third && arr[i]!= second){
+        third = arr[i];
     }
 }
-if( second === -Infinity){
-    console.log("no 2 ele");
+console.log(first);
+console.log(second)
+console.log(third);
+if(third == -Infinity){
+    console.log("NO 3rd element")
 }
